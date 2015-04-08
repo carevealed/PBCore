2a55fcb8aa4be32d5647194d2b11169baf802ab5
henryborchers <henryborchers@yahoo.com>
2015-04-08 16:02:14 -0700

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from xml.etree. ElementTree import ElementTree, Element, tostring
-
-root = Element("person")
-tree = ElementTree(root)
-root.text = "s"
-# root.append(PB_Element("name"))
-# print tree
-
-print tostring(root)
