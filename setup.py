a0da9e1d746a82efe93cd0e8612821baf10a08a9
henryborchers <henryborchers@yahoo.com>
2015-04-10 12:58:48 -0700

    upped the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='CAVPP_PBCore_Tools',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     url='https://github.com/cavpp/PBCore',
     install_requires=['OneSheet == 0.1.3.26'],
