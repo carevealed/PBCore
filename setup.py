ee2f04b7fb6d1ecd3c0ce015b43dfdc480d13d77
henryborchers <henryborchers@yahoo.com>
2015-05-22 15:08:12 -0700

    Works with silent videos now

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except(ImportError):
 
 setup(
     name='CAVPP_PBCore_Tools',
-    version='0.1.3.4',
+    version='0.1.3.6',
     packages=find_packages(),
     url='https://github.com/cavpp/PBCore',
     install_requires=['OneSheet >= 0.1.3.26', 'lxml >= 3.4.2', 'pymediainfo >=1.4.0'],
