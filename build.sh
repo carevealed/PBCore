f54a1bd914935e80c7a6f9e3b2b521b6e15e6cab
Henry <henryborchers@yahoo.com>
2015-07-02 15:03:59 -0700

    compiles lame.

--- build.sh
+++ build.sh
@@ -1,12 +1,2 @@
-#!/usr/bin
-version = "3.98.4/lame-3.98.4.tar.gz"
-
-mkdir -vp ${PREFIX}/lame;
-wget ${PREFIX}/lame/lame.tar.gz http://downloads.sourceforge.net/project/lame/lame/$version
-cd ${PREFIX}/lame;
-tar xvfz lame.tar.gz
-cd cd ${PREFIX}/lame/lame*;
-./configure
-make
-make install
-python setup.py install
+#!/bin/bash
+python setup.py install;
