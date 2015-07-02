b86213c20f375228af97fa10c3287d2463ce782e
henryborchers <henryborchers@yahoo.com>
2015-07-02 12:50:14 -0700

    cleanup

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-
+#wget http://downloads.sourceforge.net/project/lame/lame/3.98.4/lame-3.98.4.tar.gz
+#cd lame-3.98.4.tar.gz
+#./configure
+#make
+#make install
 python setup.py install
