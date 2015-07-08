4c11371537f982e24754f996955312c609200d1e
henryborchers <henryborchers@yahoo.com>
2015-07-08 13:37:45 -0700

    Moved documentation

--- build.sh
+++ build.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
+#if [ -f ${PREFIX}/ffprobe ];
+#then
+#    echo "ffprobe already installed"
+#else
 
-python setup.py install;
+#
+#
+#if [ ! -d ${SRC_DIR}/ffmpeg ];
+#then
+#    git clone git://source.ffmpeg.org/ffmpeg.git ffmpeg
+#else
+#    echo "Found FFMPEG"
+#    (cd ${SRC_DIR}/ffmpeg/; git pull)
+#fi
+#
+#(cd /${SRC_DIR}/ffmpeg/; ./configure --prefix=${PREFIX} --disable-ffmpeg  --disable-ffplay --disable-ffserver --disable-doc --enable-small; make; make install);
+#cp $(find ${PREFIX} -name ffprobe) ${PREFIX}/bin
+$PYTHON setup.py install;
+
+#fi
+
+
+#echo $PREFIX
